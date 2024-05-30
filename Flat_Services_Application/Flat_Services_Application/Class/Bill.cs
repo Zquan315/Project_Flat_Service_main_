1122a8d2e14c3e4af61233fd01b7e0ac64508768
Zquan315 <22521190@gm.uit.edu.vn>
2024-05-30 15:10:44 +0700

    tao firebase bill v them class bill

---
+++ Flat_Services_Application/Flat_Services_Application/Class/Bill.cs
@@ -0,0 +1,33 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flat_Services_Application.Class
+{
+    public class bill
+    {
+        [FirestoreProperty]
+        public string Room_price { get; set; }
+        [FirestoreProperty]
+        public int amount_Elec { get; set; }
+        [FirestoreProperty]
+        public int amount_water { get; set; }
+        [FirestoreProperty]
+        public string clean { get; set; }
+        [FirestoreProperty]
+        public string debit { get; set; }
+        [FirestoreProperty]
+        public string elec { get; set; }
+        [FirestoreProperty]
+        public string secu { get; set; }
+        [FirestoreProperty]
+        public string service { get; set; }
+        [FirestoreProperty]
+        public string total { get; set; }
+        [FirestoreProperty]
+        public string water { get; set; }
+    }
+}
