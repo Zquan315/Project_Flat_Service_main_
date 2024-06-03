9260c6bf8de0a22980f4a70af099b2ecf7c788c9
Zquan315 <22521190@gm.uit.edu.vn>
2024-06-03 21:07:47 +0700

    tao server chat

---
+++ Flat_Services_Application/ServerChat/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ServerChat
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMess = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvMess
+            // 
+            this.lvMess.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvMess.HideSelection = false;
+            this.lvMess.Location = new System.Drawing.Point(12, 39);
+            this.lvMess.Name = "lvMess";
+            this.lvMess.Size = new System.Drawing.Size(776, 373);
+            this.lvMess.TabIndex = 4;
+            this.lvMess.UseCompatibleStateImageBehavior = false;
+            this.lvMess.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Message receive";
+            this.columnHeader1.Width = 730;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvMess);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMess;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Flat_Services_Application/ServerChat/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace ServerChat
+{
+    public partial class Server : Form
+    {
+        FirestoreDb db;
+        private delegate void SafeCallDelegate(string text);
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"flatservice-a087e-firebase-adminsdk-e8i8j-118340432f.json";
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+                db = FirestoreDb.Create("flatservice-a087e");
+            }
+            catch
+            {
+                MessageBox.Show("Cann't connect to firestore!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                Thread serverThread = new Thread(StartUnsafeThread);
+                serverThread.IsBackground = true;
+                serverThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting server... " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        int flag = 0;
+        private List<Thread> clientThreads = new List<Thread>();
+        private List<NetworkStream> clientStreams = new List<NetworkStream>();
+        NetworkStream clientStream;
+        public void StartUnsafeThread()
+        {
+            try
+            {
+                Socket listenerSocket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                );
+                IPEndPoint ipE = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                listenerSocket.Bind(ipE);
+                listenerSocket.Listen(-1);
+
+                while (true)
+                {
+                    Socket clientSocket = listenerSocket.Accept();
+
+                    if (clientSocket != null)
+                    {
+                        lvMess.Items.Add(new ListViewItem("New client connected from: " + clientSocket.RemoteEndPoint.ToString() + "\n"));
+                        clientStream = new NetworkStream(clientSocket);
+                        clientStreams.Add(clientStream);
+                        Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                        clientThreads.Add(clientThread);
+                        flag++;
+                        clientThread.IsBackground = true;
+                        clientThread.Start();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in server thread: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void HandleClient(Socket clientSocket)
+        {
+            try
+            {
+                while (clientSocket.Connected)
+                {
+                    int ByteReceive = 0;
+                    byte[] rec = new byte[1];
+                    string mess = "";
+                    do
+                    {
+                        ByteReceive = clientSocket.Receive(rec);
+                        mess += Encoding.UTF8.GetString(rec);
+                    } while (mess[mess.Length - 1] != '\n');
+
+
+
+                    if (mess == "00110001000000010010000000000100\n")
+                    {
+                        lvMess.Items.Add(new ListViewItem("A Client is disconnected!\n"));
+                        flag--;
+                    }
+                    else
+                    {
+                        lvMess.Items.Add(new ListViewItem(mess));
+                        add_Chat(mess);
+                        byte[] data = Encoding.UTF8.GetBytes(mess);
+                        List<NetworkStream> connectedStreams = new List<NetworkStream>();
+                        foreach (NetworkStream stream in clientStreams)
+                        {
+                            if (stream.CanWrite)
+                            {
+                                connectedStreams.Add(stream);
+                            }
+                        }
+
+                        foreach (NetworkStream stream in connectedStreams)
+                        {
+                            stream.Write(data, 0, data.Length);
+                            stream.Flush();
+                        }
+
+                    }
+
+                    if (flag == 0)
+                    {
+                        lvMess.Items.Add(new ListViewItem("All Clients are disconnected!\n"));
+
+                        break;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                //MessageBox.Show("Error handling client: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                clientSocket.Close();
+                clientStreams.Remove(clientStream);
+            }
+        }
+
+        async void add_Chat(string s)
+        {
+            string a = "";
+            foreach (char c in s)
+            {
+                if (c != '\n')
+                    a += c;
+            }
+            DateTime now = DateTime.Now;
+
+            // Định dạng thời gian theo dạng "giờ, dd/MM/yyyy"
+            string formattedDateTime = now.ToString("HH:mm, dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DocumentReference dr = db.Collection("Chat").Document("Chat");
+            await dr.UpdateAsync("Chat", FieldValue.ArrayUnion(a + "-" + formattedDateTime));
+        }
+    }
+}
